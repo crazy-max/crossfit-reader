280a01a47e5e732119db5c150fd60d55c28c49b5
CrazyMax <crazy-max@users.noreply.github.com>
2016-09-26 01:20:46 +0200

    Quite wget

--- src-build/travisInstallJre.sh
+++ src-build/travisInstallJre.sh
@@ -7,7 +7,7 @@ echo "* ExtractPath: $extractPath"
 echo "* JreUrl: $jreUrl"
 
 cd $extractPath
-wget -c -O jre.tar.gz --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "$jreUrl"
+wget -q -c -O jre.tar.gz --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "$jreUrl"
 tar zxvf jre.tar.gz
 rm -f jre.tar.gz
 mv jre* jre
