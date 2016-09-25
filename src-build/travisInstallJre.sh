319ce0069f0712103065a46ce1c3a72b92bb495f
crazy-max <webmaster@crazyws.fr>
2016-09-26 01:12:17 +0200

    Test install JRE

--- src-build/travisInstallJre.sh
+++ src-build/travisInstallJre.sh
@@ -8,7 +8,6 @@ echo "* JreUrl: $jreUrl"
 
 cd $extractPath
 wget -c -O jre.tar.gz --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "$jreUrl"
-ls -al
-cat jre.tar.gz
-file jre.tar.gz
 tar zxvf jre.tar.gz
+rm -f jre.tar.gz
+mv jre* jre
