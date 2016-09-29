173846eb63f775add9e68e2130774808e91b933b
crazy-max <webmaster@crazyws.fr>
2016-09-29 23:19:31 +0200

    Add switchJdk bash

---
+++ src-build/travisSwitchJdk.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo "Start travisSwitchJdk..."
+
+jdkUrl=`sed '/^\#/d' build.properties | grep '^travis.jdk.url' | tail -n 1 | cut -d "=" -f2- | sed 's/^[[:space:]]*//;s/[[:space:]]*$//'`
+echo "jdkUrl: $jdkUrl"
+
+wget --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" $jdkUrl -O /tmp/jdk.tar.gz"
+tar -xzvf /tmp/jdk.tar.gz -C $HOME
+echo "export JAVA_HOME=$HOME/jdk1.8.0_66" >> ~/.bash_profile
+echo "export PATH=$PATH:$JAVA_HOME/bin" >> ~/.bash_profile
+
+source ~/.bash_profile
+echo "JAVA_HOME: $JAVA_HOME"
+echo "PATH: $PATH"
