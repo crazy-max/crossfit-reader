83a6fecadbbe08fceaf7efbaa05d0f1726eb4a56
crazy-max <webmaster@crazyws.fr>
2016-09-29 23:34:11 +0200

    Change Ant output folder

--- src-build/travisSwitchJdk.sh
+++ src-build/travisSwitchJdk.sh
@@ -6,9 +6,11 @@ echo "jdkUrl: $jdkUrl"
 
 wget --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" $jdkUrl -O /tmp/jdk.tar.gz
 tar -xzvf /tmp/jdk.tar.gz -C $HOME
-echo "export JAVA_HOME=$HOME/jdk1.8.0_66" >> ~/.bash_profile
+mv $HOME/jdk* $HOME/jdk
+echo "export JAVA_HOME=$HOME/jdk" >> ~/.bash_profile
 echo "export PATH=$PATH:$JAVA_HOME/bin" >> ~/.bash_profile
 
-source ~/.bash_profile
+export JAVA_HOME=$HOME/jdk
+export PATH=$PATH:$JAVA_HOME/bin
 echo "JAVA_HOME: $JAVA_HOME"
 echo "PATH: $PATH"
