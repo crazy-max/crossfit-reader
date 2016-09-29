ddc14fece910e49cc8f78af845f3ca5a1dcdd829
CrazyMax <crazy-max@users.noreply.github.com>
2016-09-29 23:23:37 +0200

    Syntax

--- src-build/travisSwitchJdk.sh
+++ src-build/travisSwitchJdk.sh
@@ -4,7 +4,7 @@ echo "Start travisSwitchJdk..."
 jdkUrl=`sed '/^\#/d' build.properties | grep '^travis.jdk.url' | tail -n 1 | cut -d "=" -f2- | sed 's/^[[:space:]]*//;s/[[:space:]]*$//'`
 echo "jdkUrl: $jdkUrl"
 
-wget --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" $jdkUrl -O /tmp/jdk.tar.gz"
+wget --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" $jdkUrl -O /tmp/jdk.tar.gz
 tar -xzvf /tmp/jdk.tar.gz -C $HOME
 echo "export JAVA_HOME=$HOME/jdk1.8.0_66" >> ~/.bash_profile
 echo "export PATH=$PATH:$JAVA_HOME/bin" >> ~/.bash_profile
